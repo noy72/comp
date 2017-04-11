123bda91cbf0543d63b8ad5783f3d380b6634724
野井 <kuma.tola3@gmail.com>
2017-04-11 12:14:45 +0900

    OPTINDを保持するように変更

--- co.sh
+++ co.sh
@@ -1,29 +1,34 @@
-#!/bin/sh
+function co(){
+    set -u
+    RUN=FALSE
+    NEKO=FALSE
+    DEBUG=FALSE
+    OPT=""
+    OPTIND_SAVE=$OPTIND
+    VALUE=""
 
-RUN=FALSE
-NEKO=FALSE
-DEBUG=FALSE
-OPT=
-VALUE=
-
-while getopts ang OPT
+    while getopts ang OPT
     do
         case $OPT in
             a) RUN=TRUE ;;
             n) NEKO=TRUE ;;
             g) DEBUG=TRUE ;;
             /?) echo "Usage: $0 [-ang] parameter" 1>&2
-               exit 1 ;;
+                exit 1 ;;
         esac
     done
     shift `expr $OPTIND - 1`
 
-if [ "$RUN" = "TRUE" ]; then
-    g++ $1 -std=c++11 && echo "input" && ./a.out
-elif [ "$NEKO" = "TRUE" ]; then
-    cat $1 | pbcopy
-elif [ "$DEBUG" = "TRUE" ]; then
-    g++ $1 -g -std=c++11 && lldb a.out
-else
-    g++ $1 -std=c++11
-fi
+    if [ "$RUN" = "TRUE" ]; then
+        g++ $1 -std=c++11 && echo "input" && ./a.out
+    elif [ "$NEKO" = "TRUE" ]; then
+        cat $1 | pbcopy
+    elif [ "$DEBUG" = "TRUE" ]; then
+        g++ $1 -g -std=c++11 && lldb a.out
+    else
+        echo "only"
+        g++ $1 -std=c++11
+    fi
+
+    OPTIND=$OPTIND_SAVE
+}
