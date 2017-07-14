ff9e4f5d1a8a274af6b83fc7899ffe2272cbfbcc
野井 <kuma.tola3@gmail.com>
2017-07-14 14:48:40 +0900

    一時

--- co.sh
+++ co.sh
@@ -1,4 +1,3 @@
-function co(){
     set -u
     RUN=FALSE
     NEKO=FALSE
@@ -20,15 +19,14 @@ function co(){
     shift `expr $OPTIND - 1`
 
     if [ "$RUN" = "TRUE" ]; then
-        g++ $1 -std=c++11 && echo "input" && ./a.out
+        g++ $1 -std=c++14 -Wall -fsanitize=undefined -D_GLIBCXX_DEBUG && echo "input" && ./a.out
     elif [ "$NEKO" = "TRUE" ]; then
         cat $1 | pbcopy
     elif [ "$DEBUG" = "TRUE" ]; then
-        g++ $1 -g -std=c++11 && lldb a.out
+        g++ $1 -g -std=c++14 && lldb a.out
     else
         echo "only"
         g++ $1 -std=c++11
     fi
 
     OPTIND=$OPTIND_SAVE
-}
